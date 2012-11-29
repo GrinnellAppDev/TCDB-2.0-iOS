956bc8736831f10e28c6414b26b116f124e2acca
Colin Tremblay <tremblay@grinnell.edu>
2012-11-28 21:03:44 -0600

    Initial Commit + a top bar
    placeholder buttons

---
+++ TCDB2/TCDB2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TCDB2
+//
+//  Created by Colin Tremblay on 11/28/12.
+//  Copyright (c) 2012 GrinnellAppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TCDB2/TCDB2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TCDB2
+//
+//  Created by Colin Tremblay on 11/28/12.
+//  Copyright (c) 2012 GrinnellAppDev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
